df135623a3883bd0c2334e767e69ad77d8521c07
AlexAngel102 <61982160+AlexAngel102@users.noreply.github.com>
2020-08-10 15:05:48 +0300

    Add items list

--- items.php
+++ items.php
@@ -1,1 +1,21 @@
 <?php
+$items = [
+    [
+        'item' => 'MacBook',
+        'model' => 'Pro Touch Bar 16" 1Tb 2019',
+        'type' => 'Laptop',
+        'price' => '3570$',
+    ],
+    [
+        'item' => 'iPhone',
+        'model' => '11 ProMax 512GB',
+        'type' => 'SmartPhone',
+        'price' => '1740$',
+    ],
+    [
+        'item' => 'AirPods',
+        'model' => 'Pro',
+        'type' => 'TWS Earphones',
+        'price' => '280$',
+    ]
+];
